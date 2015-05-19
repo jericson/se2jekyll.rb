23f7aa944bfb46c9d8af39a21a01da80718314bf
Jon Ericson <jlericson@gmail.com>
2015-05-19 10:19:57 -0700

    Initial release.

---
+++ se2jekyll.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'json'
+require 'htmlentities'
+require 'Date'
+
+abort('Usage: ' + $0 + ' site id[...]') unless ARGV.length >= 2
+
+site = ARGV.shift
+ARGV.each do | id |
+  # http://api.stackexchange.com/docs/posts-by-ids#filter=!*7PYFiVwh*N4PkCdfxnM3de0s50u
+  uri = URI('http://api.stackexchange.com/2.2/posts/' + id)
+  uri.query = URI.encode_www_form({ :site => site, :filter => '!*7PYFiVwh*N4PkCdfxnM3de0s50u' })
+
+  items = JSON.parse(Net::HTTP.get_response(uri).body)['items']
+
+  items.each do | post |
+    owner = post['owner']
+    author = '<a alt="' + owner['display_name']+ '" href="' + owner['link'] + '">' + owner['display_name'] + '</a>'
+
+    body = HTMLEntities.new.decode post['body_markdown']
+
+    created = DateTime.strptime(post['creation_date'].to_s, '%s')
+  
+    puts <<MD
+---
+layout: post
+title: #{ post['title'] }
+tags: meta-post 
+license: http://creativecommons.org/licenses/by-sa/3.0/
+author: #{ author }
+date: #{ created.strftime('%F %T') }
+comments: no
+---
+
+(Originally published on [#{ site} Stack Exchange](#{ post['share_link'] }/#{ owner['user_id'] }) by #{ author }.)
+
+#{ body  }
+
+MD
+  end
+end
