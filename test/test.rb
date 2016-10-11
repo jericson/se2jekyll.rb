9ef7c6c2393848dfc4b97896e35a1f8c4cd1d639
Jon Ericson <jlericson@gmail.com>
2016-10-10 21:27:08 -0700

    Ooops. Gotta actually md5 the output.

--- test/test.rb
+++ test/test.rb
@@ -4,7 +4,8 @@ require 'digest/md5'
 class TestSE2J < Minitest::Test
 
   def test_xml_regex
-    md5 = Digest::MD5.hexdigest(`ruby se2jekyll.rb stackoverflow 1732454 | md5`)
-    assert_equal md5, "d52540aba8c5a3ab1c48345160fc53ae"
+    md5 = Digest::MD5.hexdigest(`ruby se2jekyll.rb stackoverflow 1732454`)
+    puts(md5)
+    assert_equal md5, "52e9cadba02b9d4faab99c672855416b"
   end
 end
