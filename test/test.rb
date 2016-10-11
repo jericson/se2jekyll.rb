b7783d7078f48fc647381b58ac95928e2de2a7fd
Jon Ericson <jlericson@gmail.com>
2016-10-10 20:33:02 -0700

    md5 is more portable?

--- test/test.rb
+++ test/test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 class TestSE2J < Minitest::Test
 
   def test_xml_regex
-    assert_equal `ruby se2jekyll.rb stackoverflow 1732454 | cksum`,
-                 "2904996530 4680\n"
+    assert_equal `ruby se2jekyll.rb stackoverflow 1732454 | md5`,
+                 "52e9cadba02b9d4faab99c672855416b\n"
   end
 end
